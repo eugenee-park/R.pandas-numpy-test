b1c9cb8a8a18cca7d8b9512013846fd7da9111e1
sjw2332 <sjw2332@gmail.com>
2021-10-26 15:41:27 +0900

    타겟 데이터 만들기

--- test.py
+++ test.py
@@ -21,4 +21,10 @@ fish_length = np.concatenate((bream_length, smelt_length))
 fish_weight = np.concatenate((bream_weight, smelt_weight))
 
 fish_data = np.column_stack((fish_length, fish_weight))
-print(fish_data)
+#print(fish_data)
+
+#print(bream_length.shape)
+#print(smelt_length.shape)
+
+fish_target = np.concatenate((np.ones(35), np.zeros(14)))
+print(fish_target)
